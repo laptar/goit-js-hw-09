70354b58c9111e755210f009aad1e78c53df11d7
Taras Lapytskyi <84970031+laptar@users.noreply.github.com>
2022-06-22 23:32:12 +0300

    1and2

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,21 @@
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+stopBtn.disabled = true;
+let bgColor = null;
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+function changeBgColor() {
+  document.body.style.backgroundColor = `${getRandomHexColor()}`;
+}
 
+startBtn.addEventListener('click', () => {
+  bgColor = setInterval(changeBgColor, 1000);
+  startBtn.disabled = true;
+  stopBtn.disabled = false;
+});
+stopBtn.addEventListener('click', () => {
+  clearInterval(bgColor);
+  startBtn.disabled = false;
+  stopBtn.disabled = true;
+});
---
+++ src/js/02-timer.js
@@ -0,0 +1,57 @@
+import Notiflix from 'notiflix';
+
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
+
+const startBtn = document.querySelector('[data-start]');
+startBtn.disabled = true;
+
+let endDate;
+
+flatpickr('#datetime-picker', {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    if (selectedDates[0] > new Date()) {
+      endDate = selectedDates[0];
+      startBtn.disabled = false;
+      Notiflix.Notify.success('Correct date. Please click the start button.');
+    } else {
+      startBtn.disabled = true;
+      Notiflix.Notify.failure('Please choose a date in the future.');
+      // window.alert('Please choose a date in the future');
+    }
+  },
+});
+
+function convertMs(ms) {
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  const days = Math.floor(ms / day);
+  const hours = Math.floor((ms % day) / hour);
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+  return { days, hours, minutes, seconds };
+}
+
+startBtn.addEventListener('click', () => {
+  setInterval(() => {
+    document.querySelector('[data-days]').textContent = convertMs(
+      endDate - new Date()
+    ).days;
+    document.querySelector('[data-hours]').textContent = convertMs(
+      endDate - new Date()
+    ).hours;
+    document.querySelector('[data-minutes]').textContent = convertMs(
+      endDate - new Date()
+    ).minutes;
+    document.querySelector('[data-seconds]').textContent = convertMs(
+      endDate - new Date()
+    ).seconds;
+  }, 1000);
+});
