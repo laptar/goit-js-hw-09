1bdebb93aed17046a0bce7838f1a7704b6d9bec1
laptar <84970031+laptar@users.noreply.github.com>
2022-06-22 20:32:47 +0000

    Deploying to gh-pages from @ laptar/goit-js-hw-09@70354b58c9111e755210f009aad1e78c53df11d7 🚀

---
+++ 01-color-switcher.e82c4c89.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]");e.disabled=!0;var a=null;function n(){document.body.style.backgroundColor="".concat("#".concat(Math.floor(16777215*Math.random()).toString(16)))}t.addEventListener("click",(function(){a=setInterval(n,1e3),t.disabled=!0,e.disabled=!1})),e.addEventListener("click",(function(){clearInterval(a),t.disabled=!1,e.disabled=!0}))}();
+//# sourceMappingURL=01-color-switcher.e82c4c89.js.map
